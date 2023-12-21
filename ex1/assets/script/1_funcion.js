3f4e369158d426c5951a31b0c0eb2f96dc452280
ZUECO <147455197+ZUECO@users.noreply.github.com>
2023-12-20 22:16:33 -0300

    update

---
+++ ex1/assets/script/1_funcion.js
@@ -0,0 +1,13 @@
+// Elementos del DOM
+const bx1 =document.getElementById("box")
+const bx2 =document.getElementById("box2")
+const bx3 =document.getElementById("box3")
+console.log("bx1")
+// Variables
+
+// Funciones
+let function example(a, b, c){
+    return a+b+c
+    }
+// Eventos
+
